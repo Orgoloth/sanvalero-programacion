d8bcec49fd6901009a26f73967c647c28f4d7cb4
Orgoloth <orgoloth@gmail.com>
2021-02-18 23:33:11 +0100

    Commit inicial

---
+++ src/main/java/edu/sanvalero/manuel/ejercicio1/Ejercicio1.java
@@ -0,0 +1,7 @@
+package edu.sanvalero.manuel.ejercicio1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo, ejercicio 1 de Manuel");
+    }
+}
