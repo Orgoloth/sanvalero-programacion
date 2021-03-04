0b1cd0d498a41116ce850c3e869178032bcc612e
Orgoloth <orgoloth@gmail.com>
2021-03-04 14:56:34 +0100

    .

--- src/main/java/edu/sanvalero/manuel/actividadaprendizaje2/gestion/domain/gangs/Gang.java
+++
@@ -1,5 +0,0 @@
-package edu.sanvalero.manuel.actividadaprendizaje2.gestion.domain.gangs;
-
-public class Gang {
-    
-}
---
+++ src/main/java/edu/sanvalero/manuel/actividadaprendizaje2/gestion/domain/groups/Group.java
@@ -0,0 +1,5 @@
+package edu.sanvalero.manuel.actividadaprendizaje2.gestion.domain.groups;
+
+public class Group {
+    
+}
